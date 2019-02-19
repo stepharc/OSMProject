5299af05146d0da2d0b2edfd3499b8f6d8eb9578
Stéphane ARCELLIER <36193013+stepharc@users.noreply.github.com>
2019-02-19 15:06:48 +0100

    [19-02] Updated code comments
    Mise à jour des commentaires code pour support de Doxygen.

--- OSMProject/Source/OSMProject/PlayerPawn.h
+++ OSMProject/Source/OSMProject/PlayerPawn.h
@@ -6,7 +6,10 @@
 #include "GameFramework/Pawn.h"
 #include "PlayerPawn.generated.h"
 
-//DECLARE_DELEGATE_OneParam(ActorDelegate, AActor *);
+//! PlayerPawn class.
+/*!
+ * This is the class which manages the player camera in the viewer.
+ */
 
 UCLASS()
 class OSMPROJECT_API APlayerPawn : public APawn
@@ -14,44 +17,122 @@ class OSMPROJECT_API APlayerPawn : public APawn
 	GENERATED_BODY()
 
 private:
+	//! DoTrace member.
+	/*!
+	 * \param RV_Hit a pointer in which it'll contain results of the raycasting.
+	 * \param RV_TraceParams parameters for the trace.
+	 * \return true if there's at least one intersected object as a result of the raytracing.
+	 * false otherwise.
+	 * Cast a ray from the user's camera position and orientation : its length
+	 * is equal to InteractionDistance variable. Store results of the raycasting
+	 * in RV_Hit parameter.
+	 */
 	bool DoTrace(FHitResult* RV_Hit, FCollisionQueryParams* RV_TraceParams);
 
 protected:
-	//Spring arms are a good, simple way to attach a camera (or other Components) so that they're not overly rigid and feel more fluid as they move.
+	
 	UPROPERTY(EditAnywhere)
-	class USpringArmComponent* CameraSpringArm;
+	class USpringArmComponent* CameraSpringArm; /*!< //Spring arms are a good, simple way to attach a camera (or other Components) so that they're not overly rigid and feel more fluid as they move. */
 	class UCameraComponent* Camera;
 
 	//Input variables
-	FVector2D MovementInput;
-	FVector2D CameraInput;
-	float ZoomFactor;
-	bool bZoomingIn;
+	FVector2D MovementInput; /*!< For mouse movement detection. */
+	FVector2D CameraInput; /*!< For camera yaw and pitch manipulation. */
+	float ZoomFactor; /*!< For camera zoom manipulation. */
+	bool bZoomingIn; /*!< Zoom / Unzoom the camera. */
 
+	//! selectedActor variable.
+	/*!
+	 * Actor (= element) selected by the user. NULL otherwise.
+	 */ 
 	AActor * selectedActor;
+	
+	//! hoveredActor variable.
+	/*!
+	 * Actor hovered by the user's camera. NULL if the camera points on nothing.
+	 */
 	AActor * hoveredActor;
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	//Input functions
+	//! MoveForward member.
+	/*! \param AxisValue based on its value, camera will move forward or backward.
+	 */
 	void MoveForward(float AxisValue);
+	
+	//! MoveRight member.
+	/*! \param AxisValue based on its value, camera will move right or left.
+	 */
 	void MoveRight(float AxisValue);
+	
+	//! PitchCamera member.
+	/*! \param AxisValue pitch camera's future value.
+	 */
 	void PitchCamera(float AxisValue);
+	
+	//! YawCamera member.
+	/*! \param AxisValue yaw camera's future value.
+	 */
 	void YawCamera(float AxisValue);
+	
+	//! ChangeSelectedActorVisibility member.
+	/*! 
+	 * Hide or show selected actor based on its game visibility.
+	 * If there's no actor selected, this member will do nothing.
+	 */
 	void ChangeSelectedActorVisibility();
+	
+	//! ZoomIn member.
+	/*! 
+	 * Request a zoom on the user's camera.
+	 */
 	void ZoomIn();
+	
+	//! ZoomOut member.
+	/*! 
+	 * Request an unzoom on the user's camera.
+	 */
 	void ZoomOut();
+	
+	//! CastTrace member.
+	/*! 
+	 * \see DoTrace(FHitResult* RV_Hit, FCollisionQueryParams* RV_TraceParams)
+	 * \see selectedActor
+	 * Cast a ray, and analyse the results. If there's one intersected actor,
+	 * modify selectedActor variable. Otherwise, set selectedActor to NULL.
+	 */
 	void CastTrace();
+	
+	//! CastTrace member.
+	/*! 
+	 * \see DoTrace(FHitResult* RV_Hit, FCollisionQueryParams* RV_TraceParams)
+	 * \see hoveredActor
+	 * Cast a ray, and analyse the results. If there's one intersected actor,
+	 * modify hoveredActor variable. Otherwise, set hoveredActor to NULL.
+	 */
 	void CastHoverTrace();
 
 public:
-	// Sets default values for this pawn's properties
+	//! APlayerPawn constructor.
+	/*!
+	 * Set up the spring arm, the camera and player control on it.
+	 */
 	APlayerPawn();
 
-	// Called every frame
+	//! Tick member.
+	/*!
+	 * Called every frame. Check if there's need to zoom/unzoom, rotate,
+	 * translate the user's camera.
+	 */
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
+	//! SetupPlayerInputComponent member.
+	/*!
+	 * Called to bind functionality to input. 
+	 * Hook up events for "ZoomIn", "ZoomOut", "Raycast", "Hover Raycast" and "Hide/Show slected actor".
+	 * Hook up every-frame handling for camera translation, pitch and yaw.
+	 */
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
